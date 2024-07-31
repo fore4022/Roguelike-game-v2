8cb25a077c73059bdd3f76bb41438e461f0d5e1d
fore4022 <s23009@gsm.hs.kr>
2024-07-31 17:42:21 +0900

    objectPool_2
    구현 도중에 비동기 부분에서 발생한 문제 수정 중

--- Roguelike-game-v2/Assets/Scripts/ObjectPool.cs
+++ Roguelike-game-v2/Assets/Scripts/ObjectPool.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
-public class ObjectPool
+[Obsolete]
+public static class ObjectPool
 {
-    //public static Dictionary<string, >
-    public static GameObject root;
+    public static Dictionary<string, Queue<GameObject>> poolingObjects = new();
+    public static Transform root;
 
     public static void Init()
     {
@@ -11,19 +14,122 @@ public class ObjectPool
 
         if(go == null)
         {
-            root = go = new GameObject { name = "@ObjectPool" };
+            go = new GameObject { name = "@ObjectPool" };
+
+            root = go.transform;
         }
     }
-    public static void CreateObjects()
+    public static async Task CreateObjects(int count, string lable = null, string path = null)
     {
+        if(lable == path) { return; }
 
-    }
-    public static GameObject ActiveObject()
-    {
-        return null;
-    }
-    public static void DisableObject()
-    {
+        Queue<GameObject> queue;
 
+        Init();
+
+        if(path == null)
+        {
+            foreach(GameObject prefab in await Util.LoadToLable<GameObject>(lable))
+            {
+                if(poolingObjects.ContainsKey(prefab.name))
+                {
+                    queue = poolingObjects[prefab.name];
+
+                    //foreach(GameObject instance in Instantiate(prefab, count).Result)
+                    //{
+                    //    queue.Enqueue(instance);
+                    //}
+                    foreach (GameObject instance in Instantiate(prefab, count).Result)
+                    {
+                        queue.Enqueue(instance);
+                    }
+
+                    poolingObjects[prefab.name] = queue;
+                }
+                else
+                {
+                    queue = Instantiate(prefab, count).Result;
+
+                    poolingObjects.Add(prefab.name, queue);
+                }
+            }
+        }
+        else
+        {
+            GameObject prefab = await Util.LoadToPath<GameObject>(path);
+
+            if (poolingObjects.ContainsKey(prefab.name))
+            {
+                queue = poolingObjects[prefab.name];
+
+                foreach(GameObject instance in Instantiate(prefab, count).Result)
+                {
+                    queue.Enqueue(instance);
+                }
+
+                poolingObjects[prefab.name] = queue;
+            }
+            else
+            {
+                queue = Instantiate(prefab, count).Result;
+
+                poolingObjects.Add(prefab.name, queue);
+            }
+        }
     }
+    public static GameObject GetOrActiveObject(string prefabName)
+    {
+        GameObject prefab = poolingObjects[prefabName].Dequeue();
+
+        prefab.SetActive(true);
+
+        return prefab;
+    }
+    public static void ActiveObject(string prefabName)
+    {
+        GameObject prefab = poolingObjects[prefabName].Dequeue();
+
+        prefab.SetActive(true);
+    }
+    public static void DisableObject(GameObject prefab, string prefabName)
+    {
+        prefab.SetActive(false);
+
+        poolingObjects[prefabName].Enqueue(prefab);
+    }
+    public static async Task<Queue<GameObject>> Instantiate(GameObject prefab, int count)//
+    {
+        Queue<GameObject> queue = new();
+
+        await Task.Run(() =>
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    GameObject instance = GameObject.Instantiate(prefab, root);
+
+                    instance.SetActive(false);
+
+                    queue.Enqueue(instance);
+                }
+
+                return;
+            });
+
+        return queue;
+    }
+    //public static IEnumerator<Queue<GameObject>> Instantiate(GameObject prefab, int count)
+    //{
+    //    Queue<GameObject> queue = new();
+
+    //    for(int i = 0; i < count; i++)
+    //    {
+    //        GameObject instance = GameObject.Instantiate(prefab, root);
+
+    //        instance.SetActive(false);
+
+    //        queue.Enqueue(instance);
+    //    }
+
+    //    yield return queue;
+    //}
 }
