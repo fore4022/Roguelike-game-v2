6f2e9cf42c839861dec32767da69f3003b9053c5
fore4022 <s23009@gsm.hs.kr>
2024-08-02 11:59:11 +0900

    enemyDetection_1
    공격 구현을 위한 적을 감지하는 기능 구현 시작

---
+++ Roguelike-game-v2/Assets/Scripts/EnemyDetection.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+public static class EnemyDetection
+{
+    public static GameObject findNearestEnemy()
+    {
+        GameObject[] objs = findEnemiesOnScreen();
+
+        return null;
+    }
+    public static GameObject findLargestEnemyGroup()
+    {
+        GameObject[] objs = findEnemiesOnScreen();
+
+        return null;
+    }
+    public static GameObject[] findEnemiesOnScreen()
+    {
+
+
+        return null;
+    }
+}
