a5ed0d2aa81064586c8146474e918438864de702
fore4022 <s23009@gsm.hs.kr>
2024-08-02 20:31:11 +0900

    enemyDetection_2
    적 감지 기능을 위해서, 카메라 범위(overlap사용) 내의 오브젝트 반환 구현, 거리 계산 구현 중

--- Roguelike-game-v2/Assets/Scripts/EnemyDetection.cs
+++ Roguelike-game-v2/Assets/Scripts/EnemyDetection.cs
@@ -1,22 +1,69 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 public static class EnemyDetection
 {
     public static GameObject findNearestEnemy()
     {
-        GameObject[] objs = findEnemiesOnScreen();
+        List<GameObject> gameObjectArray = findEnemiesOnScreen();
 
-        return null;
+        GameObject targetObject = null;
+
+        float minDistance = 0;
+
+        foreach(GameObject go in gameObjectArray)
+        {
+            if(targetObject == null)
+            {
+                minDistance = GetDistance(go);
+                targetObject = go;
+            }
+            else if(minDistance > GetDistance(go, out float distance))
+            {
+
+            }
+        }
+
+        return targetObject;
     }
     public static GameObject findLargestEnemyGroup()
     {
-        GameObject[] objs = findEnemiesOnScreen();
+        List<GameObject> gameObjectArray = findEnemiesOnScreen();
 
-        return null;
+        GameObject targetObject = null;
+
+        foreach (GameObject go in gameObjectArray)
+        {
+
+        }
+
+        return targetObject;
     }
-    public static GameObject[] findEnemiesOnScreen()
+    public static List<GameObject> findEnemiesOnScreen()
     {
+        List<GameObject> resultArray = new List<GameObject>();
+        Collider2D[] colliderArray = null;
 
+        float y = Camera.main.orthographicSize * 2;
+        float x = y * Camera.main.aspect;
 
-        return null;
+        Vector2 cameraSize = new Vector2(x, y);
+
+        int count = Physics2D.OverlapBoxNonAlloc(Vector2.zero, cameraSize, 0, colliderArray, 0);
+
+        foreach(Collider2D col in colliderArray)
+        {
+            resultArray.Add(col.gameObject);
+        }
+
+        return resultArray;
+    }
+    public static float GetDistance(GameObject go, out float result)
+    {
+        float distance = 0;
+
+        return result = distance;
     }
 }
