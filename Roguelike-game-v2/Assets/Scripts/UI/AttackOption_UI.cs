0f3c660725ac6892347da5841bd3adbbd0624a7d
fore4022 <s23009@gsm.hs.kr>
2024-10-04 23:57:55 +0900

    attack system_7
    공격 시스템 구현 중

--- Roguelike-game-v2/Assets/Scripts/UI/AttackOption_UI.cs
+++ Roguelike-game-v2/Assets/Scripts/UI/AttackOption_UI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -8,22 +9,33 @@ public class AttackOption_UI : MonoBehaviour, IPointerEnterHandler, IPointerExit
     private Image image;
     private Animator animator;
 
+    private Coroutine adjustmentScale = null;
+
+    private const float minScale = 200;
+    private const float maxScale = 325;
+
     public void OnPointerEnter(PointerEventData eventData)
     {
+        SetAnimator(true);
 
+        StartCoroutine(SetImageScale(maxScale));
     }
     public void OnPointerExit(PointerEventData eventData)
     {
-        
+        SetAnimator(false);
+
+        StartCoroutine(SetImageScale(minScale));
     }
     private void Start()
     {
         image = GetComponent<Image>();
         animator = GetComponent<Animator>();
 
+        SetAnimator(false);
+
         gameObject.SetActive(false);
     }
-    public void SetInformation(AttackInformation_SO info)
+    public void InitOption(AttackInformation_SO info)
     {
         this.info = info;
 
@@ -38,8 +50,39 @@ public class AttackOption_UI : MonoBehaviour, IPointerEnterHandler, IPointerExit
     }
     private void OnDisable()
     {
-        image.sprite = null;
-        animator.runtimeAnimatorController = null;
+        //image.sprite = null;
+        //animator.runtimeAnimatorController = null;
         info = null;
+    }
+    private void SetAnimator(bool isEnabled)
+    {
+        animator.enabled = isEnabled;
+    }
+    private IEnumerator SetImageScale(float targetScale)
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+
+        float scale = rectTransform.localScale.x;
+        float sign;
+
+        if(targetScale > scale)
+        {
+            sign = maxScale - scale;
+        }
+        else
+        {
+            sign = scale - minScale;
+        }
+
+        Debug.Log((int)Mathf.Abs(targetScale - rectTransform.localScale.x) == 0);
+
+        while((int)Mathf.Abs(targetScale - rectTransform.localScale.x) == 0)
+        {
+            rectTransform.localScale += new Vector3(sign, sign) * Time.deltaTime;
+
+            Debug.Log(new Vector3(sign, sign) * Time.deltaTime);
+
+            yield return null;
+        }
     }
 }
