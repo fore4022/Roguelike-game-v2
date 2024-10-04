ceb559a4f832a4b342e995c4ae798fad0bb382cd
fore4022 <s23009@gsm.hs.kr>
2024-10-04 20:45:24 +0900

    attack system_6
    공격 시스템 구현 중

--- Roguelike-game-v2/Assets/Scripts/ScriptableObject/AttackInformation_SO.cs
+++ Roguelike-game-v2/Assets/Scripts/ScriptableObject/AttackInformation_SO.cs
@@ -1,8 +1,11 @@
+using UnityEditor.Animations;
 using UnityEngine;
 [CreateAssetMenu(fileName = "AttackInformation", menuName = "Create New SO/Create New AttackInformation_SO")]
 public class AttackInformation_SO : ScriptableObject
 {
     public GameObject skillObject;
+    public Sprite sprite;
+    public AnimatorController controller;
 
     public string attackName;
     public string explanation;
--- Roguelike-game-v2/Assets/Scripts/UI/AttackOption_UI.cs
+++ Roguelike-game-v2/Assets/Scripts/UI/AttackOption_UI.cs
@@ -1,20 +1,45 @@
 using UnityEngine;
-public class AttackOption_UI : MonoBehaviour
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class AttackOption_UI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private AttackInformation_SO info;
 
+    private Image image;
+    private Animator animator;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
     private void Start()
     {
+        image = GetComponent<Image>();
+        animator = GetComponent<Animator>();
+
         gameObject.SetActive(false);
     }
-    public void SetOption(AttackInformation_SO info)
+    public void SetInformation(AttackInformation_SO info)
     {
         this.info = info;
 
+        SetOption();
+
         gameObject.SetActive(true);
+    }
+    private void SetOption()
+    {
+        image.sprite = info.sprite;
+        animator.runtimeAnimatorController = info.controller;
     }
     private void OnDisable()
     {
+        image.sprite = null;
+        animator.runtimeAnimatorController = null;
         info = null;
     }
-}//IPointerEnterHandler
+}
