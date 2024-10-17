d0c681b2ca6f27122027137df0a32bad3c04919e
fore4022 <s23009@gsm.hs.kr>
2024-10-17 12:26:20 +0900

    attack caster manage_2
    모든 공격 관리

--- Roguelike-game-v2/Assets/Scripts/Attack/AttackCasterManage.cs
+++ Roguelike-game-v2/Assets/Scripts/Attack/AttackCasterManage.cs
@@ -3,16 +3,33 @@ public class AttackCasterManage
 {
     private Dictionary<string, AttackCaster> Casters = new();
 
-    public void GetCaster()
+    public AttackCaster GetCaster(string type)
     {
+        if(Casters.ContainsKey(type))
+        {
+            return Casters[type];
+        }
 
+        return default(AttackCaster);
     }
-    public void CreateCaster()
+    public void CreateCaster(string type)
     {
+        if(!Casters.ContainsKey(type))
+        {
+            AttackCaster caster = new();
 
+            caster.SetAttackType(type);
+
+            Casters.Add(type, caster);
+        }
     }
-    public void DestroyCaster()
+    public void DestroyCaster(string type)
     {
+        if(Casters.ContainsKey(type))
+        {
+            Casters[type] = null;
 
+            Casters.Remove(type);
+        }
     }
 }
