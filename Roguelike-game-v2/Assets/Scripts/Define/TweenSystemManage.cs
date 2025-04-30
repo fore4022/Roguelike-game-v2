7aff6a30f2797f6a79fad0ed6370cefe5b23bbff
fore4022 <s23009@gsm.hs.kr>
2025-04-30 20:27:14 +0900

    TweenSystem
    관리 구조 구현 중

---
+++ Roguelike-game-v2/Assets/Scripts/Define/TweenSystemManage.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+public class TweenSystemManage
+{
+    private static Tween tween = new();
+
+    private static Dictionary<Component, Queue<List<Coroutine>>> tweenSchedule = new();
+
+    private readonly Type transform = typeof(Transform);
+    private readonly Type rectTransform = typeof(RectTransform);
+
+    public void ExecuteTween(TweenType type, Component comp, FlexibleValue flexible, float duration, Ease ease = Ease.Linear)
+    {
+        Coroutine coroutine = null;
+        EaseDelegate easeDelegate = Easing.Get(ease);
+        Type t = comp.GetType();
+
+        switch (type)
+        {
+            case TweenType.Scale:
+                ScaleTweening(ref coroutine, t, comp, flexible, duration, easeDelegate);
+                break;
+            case TweenType.Rotation:
+                PositionTweening(ref coroutine, t, comp, flexible, duration, easeDelegate);
+                break;
+            case TweenType.Position:
+                RotationTweening(ref coroutine, t, comp, flexible, duration, easeDelegate);
+                break;
+        }
+
+        if (tweenSchedule.TryGetValue(comp, out Queue<List<Coroutine>> schedule))
+        {
+            schedule.Peek().Add(coroutine);
+        }
+        else
+        {
+            List<Coroutine> sched = new() { coroutine };
+
+            schedule = new();
+
+            schedule.Enqueue(sched);
+            tweenSchedule.Add(comp, schedule);
+        }
+    }
+    public void ScaleTweening(ref Coroutine coroutine, Type type, Component comp, FlexibleValue flexible, float duration, EaseDelegate ease)
+    {
+        if(type == transform)
+        {
+            coroutine = Util.GetMonoBehaviour().StartCoroutine(tween.ScaleOverTime(ease, comp as Transform, flexible.Float, duration));
+        }
+        else if(type == rectTransform)
+        {
+            coroutine = Util.GetMonoBehaviour().StartCoroutine(tween.ScaleOverTime(ease, comp as RectTransform, flexible.Float, duration));
+        }
+    }
+    public void PositionTweening(ref Coroutine coroutine, Type type, Component comp, FlexibleValue flexible, float duration, EaseDelegate ease)
+    {
+        if(type == transform)
+        {
+            coroutine = Util.GetMonoBehaviour().StartCoroutine(tween.PositionOverTime(ease, comp as Transform, flexible.Vector, duration));
+        }
+    }
+    public void RotationTweening(ref Coroutine coroutine, Type type, Component comp, FlexibleValue flexible, float duration, EaseDelegate ease)
+    {
+        if(type == transform)
+        {
+            coroutine = Util.GetMonoBehaviour().StartCoroutine(tween.RotationOverTime(ease, comp as Transform, flexible.Vector, duration));
+        }
+    }
+}
--- Roguelike-game-v2/Assets/Scripts/Util/Tweening/TweenSystem.cs
+++ Roguelike-game-v2/Assets/Scripts/Util/Tweening/TweenSystem.cs
@@ -1,65 +1,30 @@
-using System.Collections.Generic;
 using UnityEngine;
 public static class TweenSystem
 {
-    private static Tween tween = new();
-
-    private static Dictionary<Component, Queue<List<Coroutine>>> tweenSchedule = new();
+    private static TweenSystemManage manage = new();
 
     public static Transform SetScale(this Transform transform, float targetScale, float duration, Ease ease = Ease.Linear)
     { 
-        ExecuteTween(TweenType.Scale, transform, new(targetScale), duration, ease);
+        manage.ExecuteTween(TweenType.Scale, transform, new(targetScale), duration, ease);
 
         return transform;
     }
     public static RectTransform SetScale(this RectTransform rectTransform, float targetScale, float duration, Ease ease = Ease.Linear)
     {
-        Coroutine coroutine = Util.GetMonoBehaviour().StartCoroutine(tween.ScaleOverTime(Easing.Get(ease), rectTransform, targetScale, duration));
-
-        //
+        manage.ExecuteTween(TweenType.Scale, rectTransform, new(targetScale), duration, ease);
 
         return rectTransform;
     }
     public static Transform SetPosition(this Transform transform, Vector2 targetPosition, float duration, Ease ease = Ease.Linear)
     {
-        Coroutine coroutine = Util.GetMonoBehaviour().StartCoroutine(tween.PositionOverTime(Easing.Get(ease), transform, targetPosition, duration));
+        manage.ExecuteTween(TweenType.Position, transform, new(targetPosition), duration, ease);
 
         return transform;
     }
     public static Transform SetRotation(this Transform transform, Vector3 targetRotation, float duration, Ease ease = Ease.Linear)
     {
-        Coroutine coroutine = Util.GetMonoBehaviour().StartCoroutine(tween.RotationOverTime(Easing.Get(ease), transform, targetRotation, duration));
+        manage.ExecuteTween(TweenType.Rotation, transform, new(targetRotation), duration, ease);
 
         return transform;
-    }
-    private static void ExecuteTween(TweenType type, Component comp, FlexibleValue flexible, float duration, Ease ease = Ease.Linear)
-    {
-        Coroutine coroutine = null;
-
-        switch(type)
-        {
-            case TweenType.Scale:
-                coroutine = Util.GetMonoBehaviour().StartCoroutine(tween.ScaleOverTime(Easing.Get(ease), comp as Transform, flexible.Float, duration));
-                break;
-            case TweenType.Position:
-                coroutine = Util.GetMonoBehaviour().StartCoroutine(tween.ScaleOverTime(Easing.Get(ease), comp as RectTransform, flexible.Float, duration));
-                break;
-            case TweenType.Rotation:
-                break;
-        }
-
-        if(tweenSchedule.TryGetValue(comp, out Queue<List<Coroutine>> schedule))
-        {
-            schedule.Peek().Add(coroutine);
-        }
-        else
-        {
-            List<Coroutine> sched = new() { coroutine };
-
-            schedule = new();
-
-            schedule.Enqueue(sched);
-            tweenSchedule.Add(comp, schedule);
-        }
     }
 }
