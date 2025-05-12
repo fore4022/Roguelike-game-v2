c9c58defaed14fd8acfd1d62befc0db3cdb3a052
fore4022 <s23009@gsm.hs.kr>
2025-05-12 12:27:45 +0900

    타이틀 화면 구현 중
    배경 추가
    트윈 효과 추가 구현
    UI 위치 조정

--- Roguelike-game-v2/Assets/Scripts/Tweens/Title_TW.cs
+++ Roguelike-game-v2/Assets/Scripts/Tweens/Title_TW.cs
@@ -34,7 +34,7 @@ public class Title_TW : MonoBehaviour
 
         // Moth
         _mon1.SetScale(3, 0.6f, 0.2f, Ease.OutExpo)
-            .SetPosition(new(0.45f, -3.15f), 0.65f, 0.25f, Ease.OutCirc)
+            .SetPosition(new(0.45f, -3.15f), 0.65f, 0.25f, Ease.InCubic)
             .SetRotation(new(0, 0, 20), 1f, 0.25f, Ease.OutExpo);
 
         // Cloud
@@ -44,7 +44,7 @@ public class Title_TW : MonoBehaviour
 
         // SlimeSquare
         _mon3.SetScale(3, 0.9f, 0.4f, Ease.OutExpo)
-            .SetPosition(new(-2.9f, -0.8f), 1f, 0.4f, Ease.OutCirc)
+            .SetPosition(new(-2.9f, -0.8f), 1f, 0.4f, Ease.OutBack)
             .SetRotation(new(0, 0, 1120), 1.1f, 0.4f, Ease.OutExpo);
 
         // Mushroom_1
@@ -54,13 +54,13 @@ public class Title_TW : MonoBehaviour
 
         // Mushroom_2
         _mon5.SetScale(3, 1.2f, 0.45f, Ease.OutExpo)
-            .SetPosition(new(3f, -1.4f), 0.8f, 0.45f, Ease.OutExpo)
-            .SetRotation(new(0, 0, 150), 1.2f, 0.45f, Ease.OutExpo);
+            .SetPosition(new(3f, -1.4f), 0.8f, 0.45f, Ease.OutQuart)
+            .SetRotation(new(0, 0, 150), 1.2f, 0.45f, Ease.OutQuart);
 
         // Sword_1
         _mon6.SetScale(3, 1.2f, 0.55f, Ease.OutExpo)
-            .SetPosition(new(-2.25f, 6.5f), 0.8f, 0.55f, Ease.OutExpo)
-            .SetRotation(new(0, 0, 332.5f), 1.2f, 0.55f, Ease.OutExpo);
+            .SetPosition(new(-2.25f, 6.5f), 0.8f, 0.55f, Ease.OutQuad)
+            .SetRotation(new(0, 0, 332.5f), 1.2f, 0.55f, Ease.OutQuad);
 
         // Sword_2
         _mon7.SetScale(4, 1.2f, 0.45f, Ease.OutExpo)
@@ -68,9 +68,9 @@ public class Title_TW : MonoBehaviour
             .SetRotation(new(0, 0, 752.5f), 1.2f, 0.45f, Ease.OutExpo);
 
         // Mask
-        _mon8.SetScale(1.8f, 1.2f, 0.45f, Ease.OutExpo)
-            .SetPosition(new(3f, -7f), 0.8f, 0.45f, Ease.OutExpo)
-            .SetRotation(new(0, 0, 15), 1.2f, 0.45f, Ease.OutExpo);
+        _mon8.SetScale(1.8f, 1.2f, 0.45f, Ease.OutQuint)
+            .SetPosition(new(3f, -7f), 0.8f, 0.45f, Ease.OutQuint)
+            .SetRotation(new(0, 0, 15), 1.2f, 0.45f, Ease.OutQuint);
     }
     private IEnumerator ReOrder()
     {
