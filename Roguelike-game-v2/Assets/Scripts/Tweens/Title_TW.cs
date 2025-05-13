66322a5512716c6074aaef0365ced6b98c2979c5
fore4022 <s23009@gsm.hs.kr>
2025-05-13 09:27:10 +0900

    타이틀 화면 구현 중
    게임 이름 수정
    화면 내 요소들의 배치 수정

--- Roguelike-game-v2/Assets/Scripts/Tweens/Title_TW.cs
+++ Roguelike-game-v2/Assets/Scripts/Tweens/Title_TW.cs
@@ -21,6 +21,8 @@ public class Title_TW : MonoBehaviour
     [SerializeField]
     private Transform _mon8;
     [SerializeField]
+    private Transform _mon9;
+    [SerializeField]
     private SpriteRenderer _explosion;
 
     private void Start()
@@ -42,35 +44,40 @@ public class Title_TW : MonoBehaviour
             .SetPosition(new(2.2f, -4.05f), 0.65f, 0.3f, Ease.OutCirc)
             .SetRotation(new(0, 0, 7.5f), 1f, 0.3f, Ease.OutExpo);
 
-        // SlimeSquare
+        // SlimeSquare_1
         _mon3.SetScale(3, 0.9f, 0.4f, Ease.OutExpo)
             .SetPosition(new(-2.9f, -0.8f), 1f, 0.4f, Ease.OutBack)
             .SetRotation(new(0, 0, 1120), 1.1f, 0.4f, Ease.OutExpo);
 
+        // SlimeSquare_2
+        _mon4.SetScale(2.75f, 0.9f, 0.45f, Ease.OutExpo)
+            .SetPosition(new(-3.5f, 0.6f), 1f, 0.425f, Ease.OutBack)
+            .SetRotation(new(0, 0, 740), 1.1f, 0.425f, Ease.OutExpo);
+
         // Mushroom_1
-        _mon4.SetScale(2, 1.05f, 0.4f, Ease.OutExpo)
-            .SetPosition(new(3.5f, 6.1f), 1.15f, 0.4f, Ease.OutExpo)
-            .SetRotation(new(0, 0, 390), 1.25f, 0.4f, Ease.OutExpo);
+        _mon5.SetScale(2, 1.05f, 0.4f, Ease.OutExpo)
+            .SetPosition(new(3.5f, 6.1f), 1.15f, 0.45f, Ease.OutExpo)
+            .SetRotation(new(0, 0, 390), 1.25f, 0.475f, Ease.OutExpo);
 
         // Mushroom_2
-        _mon5.SetScale(3, 1.2f, 0.45f, Ease.OutExpo)
+        _mon6.SetScale(3, 1.2f, 0.45f, Ease.OutExpo)
             .SetPosition(new(3f, -1.4f), 0.8f, 0.45f, Ease.OutQuart)
             .SetRotation(new(0, 0, 150), 1.2f, 0.45f, Ease.OutQuart);
 
         // Sword_1
-        _mon6.SetScale(3, 1.2f, 0.55f, Ease.OutExpo)
-            .SetPosition(new(-3f, 6.25f), 0.8f, 0.5f, Ease.OutQuad)
-            .SetRotation(new(0, 0, 332.5f), 1.2f, 0.55f, Ease.OutQuad);
+        _mon7.SetScale(3, 1.2f, 0.55f, Ease.OutExpo)
+            .SetPosition(new(-3f, 6.75f), 0.8f, 0.5f, Ease.OutQuad)
+            .SetRotation(new(0, 0, 332.5f), 1.2f, 0.5f, Ease.OutQuad);
 
         // Sword_2
-        _mon7.SetScale(4, 1.2f, 0.45f, Ease.OutExpo)
-            .SetPosition(new(-2.5f, 4.5f), 0.8f, 0.45f, Ease.OutExpo)
+        _mon8.SetScale(4, 1.2f, 0.45f, Ease.OutExpo)
+            .SetPosition(new(-1.7f, 6.3f), 0.8f, 0.45f, Ease.OutExpo)
             .SetRotation(new(0, 0, 752.5f), 1.2f, 0.45f, Ease.OutExpo);
 
         // Mask
-        _mon8.SetScale(3.2f, 1.2f, 0.45f, Ease.OutQuint)
+        _mon9.SetScale(3.2f, 1.2f, 0.45f, Ease.OutQuint)
             .SetPosition(new(3f, -7f), 0.8f, 0.45f, Ease.OutQuint)
-            .SetRotation(new(0, 0, 15), 1.2f, 0.45f, Ease.OutQuint);
+            .SetRotation(new(0, 0, 15), 1.2f, 0.5f, Ease.OutQuint);
     }
     private IEnumerator ReOrder()
     {
