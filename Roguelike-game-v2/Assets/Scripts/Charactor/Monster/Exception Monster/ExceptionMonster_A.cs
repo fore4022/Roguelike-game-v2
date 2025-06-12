f8381ef4c31df753665638bd51ab93569ad5d182
fore4022 <s23009@gsm.hs.kr>
2025-06-12 16:00:55 +0900

    몬스터 공격 구현 중

--- Roguelike-game-v2/Assets/Scripts/Charactor/Monster/Exception Monster/ExceptionMonster_A.cs
+++ Roguelike-game-v2/Assets/Scripts/Charactor/Monster/Exception Monster/ExceptionMonster_A.cs
@@ -1,14 +1,36 @@
 using System.Collections;
+using UnityEngine;
 public class ExceptionMonster_A : BasicMonster
 {
+    [SerializeField]
+    private float duration = 2.5f;
+
+    private WaitForSeconds delay;
+    private string prefabKey;
+
     protected override void OnEnable()
     {
         base.OnEnable();
 
-        StartCoroutine(TestA());
+        StartCoroutine(RepeatBehavior());   
     }
-    private IEnumerator TestA()
+    protected override void Init()
     {
-        yield return null;
+        delay = new(duration);
+        prefabKey = monsterSO.extraObject.name;
+
+        base.Init();
+    }
+    private IEnumerator RepeatBehavior()
+    {
+        while(true)
+        {
+            yield return delay;
+
+            if(isVisible)
+            {
+                Managers.Game.objectPool.ActiveObject(prefabKey);
+            }
+        }
     }
 }
