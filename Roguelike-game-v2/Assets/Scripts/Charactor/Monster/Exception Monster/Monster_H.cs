afd598e967a52327a437becb5e96dc9698db22d9
fore4022 <s23009@gsm.hs.kr>
2025-07-13 20:23:48 +0900

    몬스터 추가 구현

--- Roguelike-game-v2/Assets/Scripts/Charactor/Monster/Exception Monster/Monster_H.cs
+++ Roguelike-game-v2/Assets/Scripts/Charactor/Monster/Exception Monster/Monster_H.cs
@@ -1,11 +1,68 @@
+using System.Collections;
+using UnityEngine;
 public class Monster_H : Monster_WithObject
 {
+    [SerializeField]
+    private string skillAnimation_Name;
+    [SerializeField]
+    private float skillDuration;
+    [SerializeField]
+    private float skillCooldown;
+    [SerializeField, Range(0, 100)]
+    private float skillCastChance;
+    [SerializeField]
+    private float skillDelay;
+
+    private const string defaultAnimation_Name = "Walk";
+
+    private WaitForSeconds cooldown;
+    private WaitForSeconds delay;
     private string skillKey;
 
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        StartCoroutine(RepeatBehavior());
+    }
     protected override void Init()
     {
+        cooldown = new(skillCooldown);
+        delay = new(skillDelay);
         skillKey = monsterSO.extraObject.name;
 
         base.Init();
+    }
+    private IEnumerator RepeatBehavior()
+    {
+        speedMultiplier = 1;
+        canSwitchDirection = true;
+
+        yield return cooldown;
+
+        if(Random.Range(0, 100) <= skillCastChance)
+        {            
+            PoolingObject go = Managers.Game.objectPool.GetObject(skillKey);
+
+            canSwitchDirection = false;
+            speedMultiplier = 0;
+
+            yield return delay;
+
+            animator.speed = 0;
+
+            speedMultiplier = 3;
+
+            yield return skillDuration;
+
+            animator.Play(skillAnimation_Name);
+            go.SetActive(true);
+
+            animator.speed = 1;
+
+            yield return new WaitUntil(() => !go.activeSelf);
+        }
+
+        StartCoroutine(RepeatBehavior());
     }
 }
--- Roguelike-game-v2/Assets/Scripts/Skill/Monster/Type/MonsterSkill_D.cs
+++ Roguelike-game-v2/Assets/Scripts/Skill/Monster/Type/MonsterSkill_D.cs
@@ -1,12 +1,26 @@
+using System.Collections;
 using UnityEngine;
-public class MonsterSkill_D : MonsterSkill_Base
+public class MonsterSkill_D : MonsterSkill_Damage
 {
+    private const float triggerTime = 0.975f;
+
     protected override void Enable()
     {
-
+        StartCoroutine(Casting());
     }
     protected override void Enter(GameObject go)
     {
-        //if()
+        if(go.TryGetComponent(out IDamageReceiver damageReceiver))
+        {
+            damageReceiver.TakeDamage(this);
+        }
+    }
+    private IEnumerator Casting()
+    {
+        SetActive(true);
+
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= triggerTime);
+
+        SetActive(false);
     }
 }
